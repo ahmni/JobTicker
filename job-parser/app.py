f74e1431a64c69d01755e3e0a92eedef1154b0e6
Ahmni <ahmni@ku.edu>
2023-10-01 11:38:58 -0400

    Parse roles, and dates added. Then output all parsed company data to a text file

--- job-parser/app.py
+++ job-parser/app.py
@@ -1,43 +1,80 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from datetime import date
 
-html_document = requests.get('https://github.com/ReaVNaiL/New-Grad-2024').text
-soup = BeautifulSoup(html_document, 'html.parser')
-#print(soup.tr)
-#print(soup.find_all('tr')[2])
-#company_list = soup[:1]
-#print(company_list)
+def rawDateToDateTime(raw_date):
+   raw_date_arr = raw_date.split("/")
+   for i, interval in enumerate(raw_date_arr):
+        raw_date_arr[i] = int(interval)
+
+   return date(raw_date_arr[2], raw_date_arr[0], raw_date_arr[1]).isoformat()
+
 class Company:
-   def __init__(self, company_name, company_link='', locations=[],
-                position_name=[], position_link=[], posted_date=None):
-       self.company_name = company_name
-       self.company_link = company_link
+   def __init__(self, name, link='', locations=[],
+                positions=[], requires_citizenship=False, posted_date=None):
+       self.name = name
+       self.link = link
        self.locations = locations
-       self.position_name = position_name
-       self.position_link = position_link
+       # tuple of postiion name, position link
+       self.positions = positions
+       self.requires_citizenship = requires_citizenship
        self.posted_date = posted_date
-# This only works because the only table is the company lists
-company_list = soup.find_all('tr')
-# remove table headers
-company_list.pop(0)
-companies = []
-for company_tag_listing in company_list:
-    listing = company_tag_listing.find_all('td')
-    # print(listing[0])
-    company_name, company_link = (listing[0].string,
-                                  listing[0].a.get('href'))
-    print(company_name, company_link)
 
-    locations = []
-    dirty_locations = listing[1].get_text().split('-')
-    if len(dirty_locations) > 1:
-        # skip empty location from delimiter
-        for location in dirty_locations[1:]:
-            locations.append(location.strip())
-    else:
-        locations = dirty_locations
-    # print(locations)
+def parseCompanyListing(link):
+    html_document = requests.get(link).text
+    soup = BeautifulSoup(html_document, 'html.parser')
 
+    # This only works because the only table is the company lists
+    company_list = soup.find_all('tr')
+    # remove table headers
+    company_list.pop(0)
+    companies = []
+    for company_tag_listing in company_list:
+        # html listing as <td> element
+        listing = company_tag_listing.find_all('td')
+        # print(listing[0])
+        company_name, company_link = (listing[0].string,
+                                      listing[0].a.get('href'))
+        # print(company_name, company_link)
 
+        locations = []
+        dirty_locations = listing[1].get_text().split('-')
+        if len(dirty_locations) > 1:
+            # skip empty location from delimiter
+            for location in dirty_locations[1:]:
+                locations.append(location.strip())
+        else:
+            locations = dirty_locations
+        # print(locations)
+
+        roles = []
+        role_html = listing[2].find_all('a')
+        for role in role_html:
+            # invalid url, potentially do something to mark these as closed in the future
+            if role.get('href') == '/ReaVNaiL/New-Grad-2024/blob/main':
+                continue
+            roles.append((role.string, role.get('href')))
+
+        if len(roles) == 0:
+            continue
+        # print(roles)
+
+        raw_date_added = listing[4].string
+        date_added = rawDateToDateTime(raw_date_added)
+
+        company = Company(company_name, company_link, locations, roles, False, date_added)
+        companies.append(company)
+
+    return companies
+
+companies = parseCompanyListing('https://github.com/ReaVNaiL/New-Grad-2024')
+
+with open('companies.txt', 'w') as file:
+    for company in companies:
+        positions = ""
+        for position in company.positions:
+            positions += position[0] + '(' + position[1] + ')' + ", "
+        positions = positions[:-1]
+        file.write(company.name + ' | ' + company.link + ' | ' + positions + ' | ' + company.posted_date + "\n")
 
