844d852a20f1fdf46611b14be82387ff8a37c9e7
Ahmni <ahmni@ku.edu>
2023-10-01 22:09:58 -0400

    Init flask server with GET requests to read company info from database

---
+++ job-parser/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+from flask_mysqldb import MySQL
+from flask_cors import CORS
+import mysql.connector
+import json
+from utils.parser import Company
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'jobTicker'
+
+mysql = MySQL(app)
+
+@app.route('/', methods = ['GET'])
+def getCompanies():
+    cursor = mysql.connection.cursor()
+    cursor.execute(""" SELECT DISTINCT * FROM companies NATURAL JOIN
+                   company_positions; """)
+    data = cursor.fetchall()
+    cursor.close()
+    res = {"body": [x for x in data]}
+
+    return json.dumps(res)
+
+@app.route('/location', methods = ['GET'])
+def getCompanyLocations():
+    args = request.args
+    company_name = args.get("company")
+
+    cursor = mysql.connection.cursor()
+    cursor.execute(""" SELECT location FROM company_locations WHERE
+                   LOWER(company_name) = LOWER(%s)""", [company_name])
+
+    data = cursor.fetchall()
+    cursor.close()
+    res = {"body": [x for x in data]}
+
+    return json.dumps(res)
+
+app.run(host='localhost', port=5000)
--- job-parser/populate-db.py
+++ job-parser/populate-db.py
@@ -14,10 +14,10 @@ def populateDB(link):
     mycursor = mydb.cursor()
 
     # create tables
-#    mycursor.execute("""CREATE TABLE companies
-#                     (name VARCHAR(255), link VARCHAR(255))
-#                     """)
-#    mycursor.execute("ALTER TABLE companies ADD UNIQUE INDEX(name)")
+    mycursor.execute("""CREATE TABLE companies
+                     (company_name VARCHAR(255), company_link VARCHAR(255))
+                     """)
+    mycursor.execute("ALTER TABLE companies ADD UNIQUE INDEX(company_name)")
 #
 #    mycursor.execute("""CREATE TABLE company_locations
 #                     (company_name VARCHAR(255), location VARCHAR(255))
@@ -25,17 +25,18 @@ def populateDB(link):
 #    mycursor.execute("""ALTER TABLE company_locations ADD UNIQUE
 #                     INDEX(company_name, location)""")
 #
-#    mycursor.execute("""CREATE TABLE company_positions
-#                     (position VARCHAR(255), company_name VARCHAR(255),
-#                      link VARCHAR(255))
-#                     """)
-#    mycursor.execute("""ALTER TABLE company_positions ADD UNIQUE
-#                     INDEX(company_name, position, link)""")
+    mycursor.execute("""CREATE TABLE company_positions
+                     (position VARCHAR(255), company_name VARCHAR(255),
+                      job_link VARCHAR(255))
+                     """)
+    mycursor.execute("""ALTER TABLE company_positions ADD UNIQUE
+                     INDEX(company_name, position, job_link)""")
 
     companies = parseCompanyListing(link)
 
     for company in companies:
-        mycursor.execute("INSERT IGNORE INTO companies (name, link) VALUES (%s, %s)",
+        mycursor.execute("""INSERT IGNORE INTO companies (company_name, company_link)
+                         VALUES (%s, %s)""",
                          (company.name, company.link))
         for location in company.locations:
             mycursor.execute("""INSERT IGNORE INTO company_locations
@@ -43,7 +44,7 @@ def populateDB(link):
                              (company.name, location))
         for position in company.positions:
             mycursor.execute("""INSERT IGNORE INTO company_positions
-                             (company_name, position, link) VALUES (%s, %s, %s)
+                             (company_name, position, job_link) VALUES (%s, %s, %s)
                              """, (company.name, position[0], position[1]))
     mydb.commit()
     print("commited to database")
